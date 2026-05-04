1e24b57886bce373bfd644076cd0c0af0c5ef171
fabrisdev <ezepuntocom@hotmail.com>
2026-05-04 00:55:22 -0300

    Improved mana bar

--- scenes/Manabar.gd
+++ scenes/Manabar.gd
@@ -1,10 +1,13 @@
 extends CanvasLayer
-export var remaining_mana = 352
+var mana_amount = 9000
+var remaining_mana = mana_amount
+const BAR_LENGTH = 352
 
 func reduce_mana(amount):
 	if remaining_mana <= 0 or amount > remaining_mana: 
 		$AnimationPlayer.play("NoMana")
 		return
-	$ConsumedMana.rect_position.x -= amount
-	$ConsumedMana.rect_size.x += amount
+	var visual_amount = range_lerp(amount, 0, mana_amount, 0, BAR_LENGTH)
+	$ConsumedMana.rect_position.x -= visual_amount
+	$ConsumedMana.rect_size.x += visual_amount
 	remaining_mana -= amount
